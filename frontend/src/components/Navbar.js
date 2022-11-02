ddb2dbbd46b1a4487f8a58da75be041e76a32ca4
Maybelline Ang <maybelline.ang@gmail.com>
2022-11-02 20:15:02 +0800

    navbar started

---
+++ frontend/src/components/Navbar.js
@@ -0,0 +1,91 @@
+import React from "react";
+import styled from "styled-components";
+import { Search, ShoppingCartOutlined } from "@material-ui/icons";
+import { Badge } from "@material-ui/core";
+
+const Container = styled.div`
+  height: 100px;
+  background-color: orange;
+`;
+
+const Wrapper = styled.div`
+  padding: 10px 20px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+const Left = styled.div`
+  flex: 3;
+  display: flex;
+  align-items: center;
+`;
+
+const SearchContainer = styled.div`
+  // border: 1px solid lightgray;
+  display: flex;
+  align-items: centre;
+  margin-left: 25px;
+  padding: 5px;
+`;
+
+const Input = styled.input`
+  border: none;
+`;
+const Language = styled.span`
+  font-size: 14px;
+  cursor: pointer;
+`;
+
+const Centre = styled.div`
+  flex: 3;
+  text-align: center;
+`;
+
+const Logo = styled.h1`
+  font-weight: bold;
+`;
+
+const Right = styled.div`
+  flex: 3;
+  display: flex;
+  align-items: center;
+  justify-content: flex-end;
+`;
+
+const MenuItem = styled.div`
+  font-size: 14px;
+  cursor: pointer;
+  margin-left: 25px;
+`;
+
+const Navbar = () => {
+  return (
+    <div>
+      <Container>
+        <Wrapper>
+          <Left>
+            <Language>EN</Language>
+            <SearchContainer>
+              <Input />
+              <Search style={{ color: "DimGray", fontSize: 20 }} />
+            </SearchContainer>
+          </Left>
+          <Centre>
+            <Logo>HELLO YOGA</Logo>
+          </Centre>
+          <Right>
+            <MenuItem>REGISTER</MenuItem>
+            <MenuItem>SIGN IN</MenuItem>
+            <MenuItem>
+              <ShoppingCartOutlined />
+              <Badge badgeContent={4} color="primary"></Badge>
+            </MenuItem>
+          </Right>
+        </Wrapper>
+      </Container>
+    </div>
+  );
+};
+
+export default Navbar;
