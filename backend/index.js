f5a09d8d66271c759d7116f3bb2a90a27672fe33
Maybelline Ang <maybelline.ang@gmail.com>
2022-11-09 23:04:21 +0800

    models completed

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
+const userRoute = require("./routes/user");
 
 dotenv.config();
 
@@ -11,6 +12,10 @@ mongoose
   .catch((err) => {
     console.log("MongoDB connection failed");
   });
+
+app.use(express.json());
+
+app.use("/api/users", userRoute);
 
 app.listen(process.env.PORT || 5001, () => {
   console.log("Backend server is running");
