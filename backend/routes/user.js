14d46e601245b336385bfb111ba8eaf805661bc9
Maybelline Ang <maybelline.ang@gmail.com>
2022-11-10 00:27:36 +0800

    jwt halfway done

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,3 +1,26 @@
 const router = require("express").Router();
+const { verifyToken, verifyTokenAuthorization } = require("./verifyToken");
+
+router.put("/:id", verifyTokenAuthorization, async (req, res) => {
+  if (req.body.password) {
+    req.body.password = CryptoJS.AES.encrypt(
+      req.body.password,
+      process.env.PASSWORD_SECRET_KEY
+    ).toString();
+  }
+
+  try {
+    const updatedUser = await User.findByIdAndUpdate(
+      req.params.id,
+      {
+        $set: req.body,
+      },
+      { new: true }
+    );
+    res.status(220).json(updatedUser);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
 module.exports = router;
---
+++ backend/routes/verifyToken.js
@@ -0,0 +1,27 @@
+const jwt = require("jsonwebtoken");
+
+const verifyToken = (req, res, next) => {
+  const authHeader = req.headers.token;
+
+  if (authHeader) {
+    jwt.verify(token, process.env.JWT_SECRET_KEY, (err, user) => {
+      if (err) res.status(403).json("Invalid Token");
+      req.user = user;
+      next();
+    });
+  } else {
+    return res.status(401).json("You are not authenticated");
+  }
+};
+
+const verifyTokenAuthorization = (req, res, next) => {
+  verifyToken(req, res, () => {
+    if (req.user.id === req.params.id || req.user.isAdmin) {
+      next();
+    } else {
+      res.status(403).json("You are unauthorised to enter");
+    }
+  });
+};
+
+module.exports = { verifyToken, verifyTokenAuthorization };
